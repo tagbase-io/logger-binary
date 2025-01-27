087993c6c6ae8877deb893d7bdc96162bff78e48
Mario Uher <uher.mario@gmail.com>
2025-01-27 17:02:16 +0100

    Enable Elixir versions back to 1.12

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule LoggerBinary.MixProject do
     [
       app: :logger_binary,
       version: "0.1.0",
-      elixir: "~> 1.16",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]

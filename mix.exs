a91ed09879bb514321cff96dd51f9502787842c8
Mario Uher <uher.mario@gmail.com>
2025-02-02 09:01:09 +0100

    Raise minimum Elixir version to 1.15

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule LoggerBinary.MixProject do
     [
       app: :logger_binary,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: "A custom Logger formatter for handling binary data.",
